69634df35a1015531023b8a252571a791e0db1bf
Nyerhovwo Adjekughene <Nerojust4@gmail.com>
2024-01-09 14:41:26 +0700

    updating label, and added validation for reset password page.

--- app/src/main/java/com/nerojust/arkandarcsadmin/admin/views/reset_password/ResetPassword.java
+++ app/src/main/java/com/nerojust/arkandarcsadmin/admin/views/reset_password/ResetPassword.java
@@ -2,6 +2,7 @@ package com.nerojust.arkandarcsadmin.admin.views.reset_password;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,25 +10,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.nerojust.arkandarcsadmin.R;
 import com.nerojust.arkandarcsadmin.utils.AppUtils;
 
+import java.util.Objects;
+
 public class ResetPassword extends AppCompatActivity {
+
+    private EditText resetPasswordEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
 
+        resetPasswordEt = findViewById(R.id.resetPasswordEt);
         Button resetButton = findViewById(R.id.resetButton);
         resetButton.setOnClickListener(v -> {
-            AppUtils.initLoadingDialog(this);
+            if (isValidFields()) {
+                AppUtils.initLoadingDialog(this);
 
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                Toast.makeText(this, "An error has occurred, please try again later", Toast.LENGTH_SHORT).show();
+                AppUtils.dismissLoadingDialog();
             }
-
-            Toast.makeText(this, "An error has occurred, please try again later", Toast.LENGTH_SHORT).show();
-            AppUtils.dismissLoadingDialog();
         });
+    }
+
+    private boolean isValidFields() {
+        if (Objects.requireNonNull(resetPasswordEt.getText()).toString().isEmpty()) {
+            Toast.makeText(this, getResources().getString(R.string.username_is_required), Toast.LENGTH_SHORT).show();
+            resetPasswordEt.requestFocus();
+            return false;
+        }
+        if (resetPasswordEt.getText().toString().length() < 3) {
+            Toast.makeText(this, getResources().getString(R.string.username_is_too_short), Toast.LENGTH_SHORT).show();
+            resetPasswordEt.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 }
